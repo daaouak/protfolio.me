cbba1f061a984939a947e300f2492f89390dfa30
AbdessamadPas <boughadoin@gmail.com>
2023-09-30 01:08:19 +0100

    setup sitemap configs🕸️

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
 // next.config.js
 module.exports = {nextConfig,
   images: {
+    unoptimized: true,
     domains: ['gatsby-starter-portfolio-minimal.netlify.app', "sendatrack.com","scontent.ftng2-1.fna.fbcdn.net","github-production-user-asset-6210df.s3.amazonaws.com" ], // Add your domain here
   },
 };
