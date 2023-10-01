e2e180fa6adc9ea818d484dcda7ae58907e060c9
AbdessamadPas <boughadoin@gmail.com>
2023-10-01 02:45:15 +0100

    add sub-domains  & tags meta in header for keywords

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,47 @@ export default function App({ Component, pageProps }: AppProps) {
           name='description'
           content='AbdessamadPas is a full stack developer based in Morocco.'
         />
+        <meta name='author' content='AbdessamadPas' />
+        <meta name='keywords' content='AbdessamadPas, Abdessamad, Pas, aalouwan, Aalouwan, abdessamadAalouwan,abdessamadpas.tech' />
+        <meta name='theme-color' content='#ffffff' />
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:site' content='@AbdessamadPas' />
+        <meta name='twitter:creator' content='@AbdessamadPas' />
+        <meta name='twitter:title' content='AbdessamadPas' />
+        <meta
+          name='twitter:description'
+          content='AbdessamadPas is a full stack developer based in Morocco.'
+        />
+        <meta name='twitter:image' content='/me.png' />
+        <meta property='og:title' content='AbdessamadPas' />
+        <meta
+          property='og:description'
+          content='AbdessamadPas is a full stack developer based in Morocco.'
+        />
+        <meta property='og:image' content='/me.png' />
+        <meta property='og:url' content='https://abdessamadpas.tech' />
+        <meta property='og:site_name' content='AbdessamadPas' />
+        <meta property='og:locale' content='en_US' />
+        <meta property='og:type' content='website' />
+        <meta property='og:author' content='AbdessamadPas' />
+        <meta property='og:published_time' content='2023-08-15' />
+        <meta property='og:modified_time' content='2023-08-15' />
+        <meta property='og:section' content='Technology' />
+        <meta property='og:tag' content='Technology' />
+        <meta property='fb:app_id' content='AbdessamadPas' />
+        <meta property='fb:profile_id' content='AbdessamadPas' />
+        <meta property='fb:author' content='AbdessamadPas' />
+        <meta property='fb:page_id' content='AbdessamadPas' />
+        <meta property='fb:admins' content='AbdessamadPas' />
+        <meta property='fb:publish_date' content='2023-08-15' />
+        <meta property='fb:modified_date' content='2023-08-15' />
+        <meta property='fb:section' content='Technology' />
+        <meta property='fb:tag' content='Technology' />
+        <meta property='article:author' content='AbdessamadPas' />
+        <meta property='article:publisher' content='AbdessamadPas' />
+        <meta property='article:published_time' content='2023-08-15' />
+        <meta property='article:modified_time' content='2023-08-15' />
+        <meta property='article:section' content='Technology' />
       </Head>
       <Layout>
         <Component {...pageProps} />
