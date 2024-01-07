e632a4e14aae09e4d5a3f1670c8fe69a5315e464
AbdessamadPas <boughadoin@gmail.com>
2024-01-07 18:24:50 +0100

    add lang eng

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
 
 // next.config.js
 module.exports = {nextConfig,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   images: {
     unoptimized: true,
     domains: ['gatsby-starter-portfolio-minimal.netlify.app', "sendatrack.com","scontent.ftng2-1.fna.fbcdn.net","github-production-user-asset-6210df.s3.amazonaws.com" ], // Add your domain here
